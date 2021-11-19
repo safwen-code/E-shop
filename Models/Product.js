17f32e372bb7487f95fb92453b3e8da618f66b77
safwen-code <safwendjebbi1234@gmail.com>
2021-11-19 18:52:09 +0100

    create the hol schema of product

--- Models/Product.js
+++ Models/Product.js
@@ -2,9 +2,54 @@ const mongoose = require("mongoose");
 const schema = mongoose.Schema;
 
 const productSchema = new schema({
-  name: String,
-  image: String,
-  CountInStock: Number,
+  name: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  richdescrption: {
+    type: String,
+    default: "",
+  },
+  image: { type: String, default: "" },
+  images: [
+    {
+      type: String,
+    },
+  ],
+  brand: {
+    type: String,
+  },
+  price: {
+    type: Number,
+    default: "0",
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "categorise",
+  },
+  countinstocke: {
+    type: Number,
+    min: 0,
+    max: 250,
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  isFeatured: {
+    type: Boolean,
+    default: false,
+  },
+  numReviews: {
+    type: Number,
+    default: 0,
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now(),
+  },
 });
 
 module.exports = Product = mongoose.model("product", productSchema);
