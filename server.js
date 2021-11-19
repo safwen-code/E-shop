531ed7b4096edfaf26c1832be0922e540150dae1
safwen-code <safwendjebbi1234@gmail.com>
2021-11-19 18:36:11 +0100

    use Cors

--- server.js
+++ server.js
@@ -1,36 +1,31 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const morgan = require('morgan')
+const morgan = require("morgan");
+const connnectDB = require("./config/connectDB");
+const cors = require("cors");
 
+//get routes
+const product = require("./Routes/product");
+const categories = require("./Routes/Categories");
+const orders = require("./Routes/ordres");
+const user = require("./Routes/user");
 //initial the app
 const app = express();
 
+app.use(cors());
+app.options("*", cors());
 //create a port
 const port = process.env.PORT || 3000;
 
+connnectDB();
 //bodyparser middelware
 app.use(express.json({ extended: false }));
-app.use(morgan('tiny'))
+app.use(morgan("tiny"));
 
-
-app.get("/product", (req, res) => {
-  const product = {
-    nameprod: "I phone",
-    price: 200,
-    quantity: 5,
-  };
-  console.log(product);
-  
-  res.send(product)
-  
-});
-
-app.post("/addproduct", (req, res) => {
-  const newProduct = req.body;
-  console.log(newProduct);
-  res.send(newProduct);
-});
-
+app.use("/", product);
+app.use("/categories", categories)
+app.use('/orders', orders)
+app.use('/user', user)
 //lunch server
 app.listen(port, () => {
   console.log(`server is work in Port ${port}`);
