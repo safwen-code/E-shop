b5ac67b350840e4250f7c044085271a072859bf1
safwen-code <safwendjebbi1234@gmail.com>
2021-11-19 17:16:25 +0100

    use morgan for logger http

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require('morgan')
+
+//initial the app
+const app = express();
 
 //create a port
 const port = process.env.PORT || 3000;
 
-const app = express();
+//bodyparser middelware
+app.use(express.json({ extended: false }));
+app.use(morgan('tiny'))
 
-app.get("/", (req, res) => {
-  res.send("hi E shop");
+
+app.get("/product", (req, res) => {
+  const product = {
+    nameprod: "I phone",
+    price: 200,
+    quantity: 5,
+  };
+  console.log(product);
+  
+  res.send(product)
+  
+});
+
+app.post("/addproduct", (req, res) => {
+  const newProduct = req.body;
+  console.log(newProduct);
+  res.send(newProduct);
 });
 
 //lunch server
