70ec9daaeee7f19d2b602babb4d70c67c38d5e5a
safwen-code <safwendjebbi1234@gmail.com>
2021-11-22 23:57:25 +0100

    add update Product

--- Routes/product.js
+++ Routes/product.js
@@ -1,13 +1,15 @@
 const express = require("express");
+const { set } = require("mongoose");
 const Categories = require("../Models/Categories");
+
 const route = express.Router();
 const Product = require("../Models/Product");
 
 route.get("/product", async (req, res) => {
   try {
-    const allProduct = await Product.find().select(
-      "name image images"
-    ).populate("category");
+    const allProduct = await Product.find()
+      .select("name image images")
+      .populate("category");
     if (!allProduct) {
       res.status(401).json({ msg: "no product" });
     }
@@ -68,6 +70,58 @@ route.post("/addproduct", async (req, res) => {
     res.status(201).json(product);
   } catch (error) {
     res.status(500).json({ msg: error.message });
+  }
+});
+
+route.put("/updateproduct/:id", async (req, res) => {
+  // check for the right categorie
+  const {
+    name,
+    description,
+    richdescrption,
+    image,
+    images,
+    brand,
+    price,
+    category,
+    countinstocke,
+    rating,
+    isFeatured,
+    numReviews,
+    dateCreated,
+  } = req.body;
+  const Categorie = await Categories.findById(req.body.category);
+
+  if (!Categorie) {
+    return res.status(404).json({ msg: "categoriy" });
+  }
+  console.log(Categorie);
+  try {
+    const product = await Product.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          name,
+          description,
+          richdescrption,
+          image,
+          images,
+          brand,
+          price,
+          category,
+          countinstocke,
+          rating,
+          isFeatured,
+          numReviews,
+          dateCreated,
+        },
+      },
+      { new: true }
+    );
+    console.log(product);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+    console.error(error.message);
   }
 });
 
