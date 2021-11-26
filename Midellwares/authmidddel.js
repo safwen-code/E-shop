f973241b847eb606f40248c08a76824cabf6f5c4
safwen-code <safwendjebbi1234@gmail.com>
2021-11-26 23:45:56 +0100

    secure my api with admin rules

--- Midellwares/authmidddel.js
+++ Midellwares/authmidddel.js
@@ -1,21 +1,27 @@
-const jwt =require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 const config = require("config");
 const secret = config.get("secret");
 
 function authmidddel(req, res, next) {
-    // get Token from header
-    const token =req.header('x-auth-token')
-    //check for token
-    if(!token) return res.status(401).json({msg:"no token authorization false"})
-    try {
+  // get Token from header
+  const token = req.header("x-auth-token");
+
+  //check for token
+  if (!token)
+    return res.status(401).json({ msg: "no token authorization false" });
+  try {
     //verifiy token
-    const decoded =jwt.verify(token, secret)
+    const decoded = jwt.verify(token, secret);
     //add user from payload
-    req.user=decoded.user
-    next()
-    } catch (e) {
-        res.status(400).json({msg:'token is not valid'})
+    req.user = decoded.user;
+    if (decoded.isAdmin) {
+      next();
+    } else {
+      return res.status(401).json({ msg: "not admin" });
     }
+  } catch (e) {
+    res.status(400).json({ msg: "token is not valid" });
+  }
 }
 
 module.exports = authmidddel;
