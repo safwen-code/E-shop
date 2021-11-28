b7657692e332da89801e0d1326730f1ad1a2256f
safwen-code <safwendjebbi1234@gmail.com>
2021-11-28 21:50:05 +0100

    add orders and ordersItem

--- Routes/ordres.js
+++ Routes/ordres.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const Order = require("../Models/Order");
+const OrderItem = require("../Models/OrderItem");
 const route = express.Router();
+
 route.get("/", async (req, res) => {
   try {
     res.status(200).json({ msg: "hi from orders" });
@@ -8,4 +11,37 @@ route.get("/", async (req, res) => {
   }
 });
 
+route.post("/", async (req, res) => {
+  const orderitemsId = Promise.all(
+    req.body.orderitems.map(async (valueItem) => {
+      let newOrderItem = new OrderItem({
+        quantity: valueItem.quantity,
+        product: valueItem.product,
+      });
+      newOrderItem = await newOrderItem.save();
+      return newOrderItem._id;
+    })
+  );
+  const orderItemresolved = await orderitemsId;
+  try {
+    const newOrder = new Order({
+      orderitems: orderItemresolved,
+      shoppingAdress: req.body.shoppingAdress,
+      shoppingAdress1: req.body.shoppingAdress1,
+      city: req.body.city,
+      zip: req.body.zip,
+      country: req.body.country,
+      phone: req.body.phone,
+      status: req.body.status,
+      totalPrice: req.body.totalPrice,
+      user: req.body.user,
+    });
+
+    await newOrder.save();
+
+    res.status(200).json({ newOrder });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+});
 module.exports = route;
